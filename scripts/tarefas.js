03cb5b8777ed59135ffdf713b5fc80ef32d3c061
geyson <geyson059@gmail.com>
2022-04-14 21:28:06 -0300

    Novas tarefas

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -13,3 +13,33 @@ const tarefas = () => {
 
 }
 tarefas()
+
+
+
+let novaTarefa = () => {
+    fetch("https://ctd-todo-api.herokuapp.com/v1/tasks", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": localStorage.getItem('jwt')
+        },
+        body: JSON.stringify({
+            "title": "Nova tarefa",
+            "description": "Nova descrição",
+            "status": "PENDING"
+        })
+    }).then(response => {
+        return response.json()
+    }).then(response => {
+        console.log(response)
+    })
+    
+}
+
+let button = document.getElementById("novaTarefa")
+button.addEventListener("click", (event) => {
+    event.preventDefault()
+    novaTarefa()
+})
+
+
